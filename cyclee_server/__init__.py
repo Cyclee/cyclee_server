8189abb5435524ff7effcd9dc84fb83971b7511a
Ivan Willig <iwillig@gmail.com>
2012-09-20 22:57:08 -0400

    Added custom render for time time objects
    Extend the default JSON render to support datetime objects. This way
    any list with the server models can be passed to the render without
    manual iterating the collection and converting the models to json by
    hand.

--- cyclee_server/__init__.py
+++ cyclee_server/__init__.py
@@ -1,8 +1,20 @@
+import datetime
 from pyramid.config import Configurator
 from sqlalchemy import engine_from_config
 
 from cyclee_server.models import DBSession
 from cyclee_server.views import RESTTrace
+
+from pyramid.renderers import JSON
+
+# FIX ME, find a better place for the adapter's for the JSON renderer
+json_renderer = JSON()
+
+
+def datetime_adapter(obj, request):
+    return obj.isoformat()
+
+json_renderer.add_adapter(datetime.datetime, datetime_adapter)
 
 
 def main(global_config, **settings):
@@ -10,17 +22,22 @@ def main(global_config, **settings):
     """
     engine = engine_from_config(settings, 'sqlalchemy.')
     DBSession.configure(bind=engine)
+    Base.metadata.create_all(engine)
     config = Configurator(settings=settings)
     config.add_static_view('static', 'static', cache_max_age=3600)
+    config.add_renderer('json', json_renderer)
+
     config.add_route('home', '/')
 
-    config.add_route('rest-traces', '/traces')
+    config.add_route('traces', '/traces')
 
     config.add_route('rest-trace', '/traces/{id}')
 
     config.add_view(RESTTrace, attr='get', request_method='GET')
     config.add_view(RESTTrace, attr='post', request_method='POST')
     config.add_view(RESTTrace, attr='delete', request_method='DELETE')
+
+    config.add_route('rides', '/rides')
 
     config.scan()
     return config.make_wsgi_app()
